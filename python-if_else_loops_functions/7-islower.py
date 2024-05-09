123fb3bfdf3490fd6fd1cc7f1ce5fbfe7a02b915
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-09 16:31:02 -0400

    l

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def islower(c: str):
+def islower(c):
+    if isinstance(c, int):
+        raise Exception
     for i in range(97, 123):
         if i == ord(str(c)):
             return True
