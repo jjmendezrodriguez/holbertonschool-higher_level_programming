3ac7e63dba6f0867a19351be83ce0f944d8696cf
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-09 13:21:20 -0400

    fix task 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 words = str.split()
 print(f"{words[5]} {words[6]} with {words[0]}")
-print(words)
