995a438ab8d29694bd749a035d414a07691b3286
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-08 23:10:27 -0400

    fix task 4

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-full_str = str1 + " " + str2  # Concatenar str1 y str2
-print(f"Welcome to {full_str}!")  # Usar la variable full_str en el print
+full_str = str1 + " " + str2  
+print(f"Welcome to {full_str}!")
