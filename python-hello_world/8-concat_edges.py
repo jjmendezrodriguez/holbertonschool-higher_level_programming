4b16e8a207a0a25092bf41b6a2fe706c49b77ce4
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-08 23:06:04 -0400

    fix tasks

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[str.find("object-oriented"):str.find("language")] + "with " + str[:6])
-print("\n")
