7bdfaa22ecb3458e58596a0d200ba2f109aa1679
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-10 11:18:05 -0400

    task 100

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+result = ""
+for i in range(122, 96, -1):
+    char = chr(i)
+    if i % 2 == 0:
+        result += char.lower()
+    else:
+        result += char.upper()
+
+print(result)
