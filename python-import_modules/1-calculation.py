b95cd1acda5f31730761a802250b6ab0fe6236fd
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-11 16:28:31 -0400

    task 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+if __name__ == "__main__":
+    addition_result = add(a, b)
+    subtraction_result = sub(a, b)
+    multiplication_result = mul(a, b)
+    division_result = div(a, b)
+    
+    print("{} + {} = {}".format(a, b, addition_result))
+    print("{} - {} = {}".format(a, b, subtraction_result))
+    print("{} * {} = {}".format(a, b, multiplication_result))
+    print("{} / {} = {}".format(a, b, division_result))
