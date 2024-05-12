78b66e9fff7b8ef0666dd5db566c9de52de68a16
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-12 02:26:46 -0400

    fix *

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,21 +2,27 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
+
 def main():
-    if len(sys.argv) != 4:
+    for i in sys.argv:
+        pass
+    if len(sys.argv) != 4 and not isinstance(int(i), int):
         print("Usage: ./100-mycalculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    if len(sys.argv[2]) != 1 and sys.argv[2] not in ['+', '-', '/']:
+        operator = chr(42)
+    else:
+        operator = sys.argv[2]
+    b = int(i)
 
     result = 0
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
         result = sub(a, b)
-    elif operator == chr(42):
+    elif operator == '*':
         result = mul(a, b)
     elif operator == '/':
         result = div(a, b)
@@ -25,5 +31,7 @@ def main():
         sys.exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
+
+
 if __name__ == "__main__":
-    main() 
+    main()
