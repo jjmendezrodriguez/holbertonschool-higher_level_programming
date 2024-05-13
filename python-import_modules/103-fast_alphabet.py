f6f27b0d0c0c3de1442c08169cb2ddc10c4c9d94
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-13 11:02:44 -0400

    fix task 9

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-print(chr(65).upper() + chr(66).upper() + chr(67).upper() + chr(68).upper() + chr(69).upper() + chr(70).upper() + chr(71).upper() + chr(72).upper() + chr(73).upper() + chr(74).upper() + chr(75).upper() + chr(76).upper() + chr(77).upper() + chr(78).upper() + chr(79).upper() + chr(80).upper() + chr(81).upper() + chr(82).upper() + chr(83).upper() + chr(84).upper() + chr(85).upper() + chr(86).upper() + chr(87).upper() + chr(88).upper() + chr(89).upper() + chr(90).upper())
+print(chr(65).upper() + chr(66).upper() + chr(67).upper() +
+       chr(68).upper() + chr(69).upper() + chr(70).upper() +
+         chr(71).upper() + chr(72).upper() + chr(73).upper() + 
+         chr(74).upper() + chr(75).upper() + chr(76).upper() + 
+         chr(77).upper() + chr(78).upper() + chr(79).upper() + 
+         chr(80).upper() + chr(81).upper() + chr(82).upper() + 
+         chr(83).upper() + chr(84).upper() + chr(85).upper() +
+         chr(86).upper() + chr(87).upper() + chr(88).upper() +
+         chr(89).upper() + chr(90).upper())
