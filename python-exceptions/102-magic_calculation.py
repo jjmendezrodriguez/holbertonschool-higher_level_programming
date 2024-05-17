4b00eb8bf34aae1e50cd41feb5a229011d3edfe2
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-17 13:55:55 -0400

    fix task 9

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    try:
-        for i in range(1, 3):
+    for i in range(1, 3):
+        try:
             if i > a:
                 raise Exception("Too far")
-            result += (a ** b) / i
-    except ValueError:
-        result = a + b
-    finally:
-        print("Inside result:", result)
+            else:
+                result += (a ** b) / i
+        except ValueError:
+            result = a + b
+            break
     return result
