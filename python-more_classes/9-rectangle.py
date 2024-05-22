a69cbcff50123a777f45daa1f9c040d1c3728c96
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-21 21:33:08 -0400

    fix def. code 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -92,4 +92,4 @@ class Rectangle:
     @classmethod
     def square(Rectangle, size=0):
         """Return a new Rectangle instance with width == height == size."""
-        return Rectangle(width=size, height=size)
+        return Rectangle(size, size)
