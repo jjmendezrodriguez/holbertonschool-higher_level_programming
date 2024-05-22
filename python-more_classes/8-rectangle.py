97a7d0a715d94c12d5ed750f234d8d912d1f1d18
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-21 20:35:06 -0400

    task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -80,11 +80,12 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         """Return the bigger rectangle based on the area or rect_1 if both have
          the same area."""
+        msg = "must be an instance of Rectangle"
+
         if not isinstance(
                 rect_1, Rectangle) or not isinstance(rect_2, Rectangle):
             raise TypeError(
-                f"{'rect_1' if not isinstance(
-                    rect_1, Rectangle) else 'rect_2'} must be an instance of Rectangle")
+                f"{'rect_1' if not isinstance(rect_1, Rectangle) else 'rect_2'} {msg}")
 
         if rect_1.area() >= rect_2.area():
             return rect_1
