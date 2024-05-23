dee2f62315b0c18b9ae8ad6202a10813e73b918c
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-22 22:55:48 -0400

    fix task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,23 +3,25 @@
 This module defines the MyList class and the lookup function.
 
 Classes:
-    MyList: Inherits from the built-in list class and provides a method to print the list sorted.
+    MyList: Inherits from the built-in list class and provides
+     a method to print the list sorted.
 Functions:
     lookup: Returns the list of available attributes and methods of an object.
 """
 
+
 class MyList(list):
     """
     MyList is a subclass of the built-in list class.
-    
+
     Methods:
         print_sorted(self): Prints the list sorted in ascending order.
     """
-    
+
     def print_sorted(self):
         """
         Prints the list sorted in ascending order.
-        
+
         Assumes all elements in the list are integers.
         """
         print(sorted(self))
@@ -33,6 +35,7 @@ def lookup(obj):
         obj: The object to inspect.
 
     Returns:
-        list: A list of strings representing the attributes and methods of the object.
+        list: A list of strings representing the attributes and
+         methods of the object.
     """
     return dir(obj)
