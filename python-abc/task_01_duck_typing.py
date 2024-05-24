25e274c3ca7da936b34bd07d292d575189bf12c6
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-24 13:15:58 -0400

    task 1 fix

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -67,11 +67,7 @@ class Rectangle(Shape):
         """
         self.width = width
         self.height = height
-        
-        if width < 0 or height < 0:
-            raise ValueError("Width and height must be positive")
-        self.width = width
-        self.height = height
+    
 
     def area(self):
         """
