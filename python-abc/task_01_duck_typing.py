1cf9eaec01a2cca3fa92220277aea156977458f9
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-24 13:15:09 -0400

    fix negative numbers. task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -67,6 +67,11 @@ class Rectangle(Shape):
         """
         self.width = width
         self.height = height
+        
+        if width < 0 or height < 0:
+            raise ValueError("Width and height must be positive")
+        self.width = width
+        self.height = height
 
     def area(self):
         """
