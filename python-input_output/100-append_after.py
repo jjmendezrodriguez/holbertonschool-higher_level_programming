551c87f86ff9c84c21433f340d62c6a50d176880
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-29 10:56:54 -0400

    fix code errors

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,18 +1,36 @@
+#!/usr/bin/python3
+"""
+This module provides a function 'append_after' that inserts a
+specific string into a file. It adds the string after each
+line that contains a specified search string. This functionality
+is useful for modifying files in a targeted way, such as adding
+comments or additional data where certain patterns or keywords exist.
+"""
+
+
 def append_after(filename="", search_string="", new_string=""):
     """
     Inserts a line of text to a file after each line containing
-    a specific string.
+    a specific string. If the file does not exist, it creates an
+    empty file first.
 
     Args:
         filename (str): The path to the file.
         search_string (str): The string to search for within the
-        file's content.
+                             file's content.
         new_string (str): The string to append after each line
-        containing the search string.
+                          containing the search string.
     """
-    # Read the file contents
-    with open(filename, 'r') as file:
-        lines = file.readlines()
+    try:
+        # Attempt to open the file and read lines
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        """If the file does not exist, create an empty
+        list of lines and also create the file"""
+        lines = []
+        with open(filename, 'w') as file:
+            file.write('')
 
     # Create a new list to store modified lines
     new_lines = []
@@ -24,6 +42,6 @@ def append_after(filename="", search_string="", new_string=""):
             # If so, append the new string as a new line
             new_lines.append(new_string)
 
-    # Write the modified lines back to the file
+    # Write the modified or new lines back to the file
     with open(filename, 'w') as file:
         file.writelines(new_lines)
