a5b014e4e08c77f660f507c05b459cc633230b94
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-29 14:45:33 -0400

    task 0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,35 @@
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize the given dictionary and save it to a JSON file.
+    
+    Parameters:
+    data (dict): The dictionary to serialize.
+    filename (str): The filename of the output JSON file. If the output file already exists, it should be replaced.
+    """
+    try:
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"Data serialized and saved to '{filename}'.")
+    except Exception as e:
+        print(f"An error occurred while saving data to '{filename}': {e}")
+
+def load_and_deserialize(filename):
+    """
+    Load JSON data from a file and deserialize it into a dictionary.
+    
+    Parameters:
+    filename (str): The filename of the input JSON file.
+    
+    Returns:
+    dict: The deserialized dictionary from the JSON file.
+    """
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        print(f"Data loaded and deserialized from '{filename}'.")
+        return data
+    except Exception as e:
+        print(f"An error occurred while loading data from '{filename}': {e}")
+        return None
