f03119577073006cc00a6b3676b3616e9c01db1d
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-05-30 23:32:43 -0400

    fix erros code 3 the else

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -51,12 +51,12 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
         else:
             # Send a 404 Not Found response for undefined endpoints
-            self.send_response("404 Not Found")
+            self.send_error(404, message= "Not Found")
             # Specify the content type as application/json
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             # Create an error message
-            error_message = {"Endpoint not found"}
+            error_message = {"error": "Endpoint not found"}
             # Write the JSON response body
             self.wfile.write(json.dumps(error_message).encode('utf-8'))
 
