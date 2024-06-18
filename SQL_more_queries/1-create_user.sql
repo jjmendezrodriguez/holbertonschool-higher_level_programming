145423a486763b77135d08cb21809fab5f158994
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-06-18 16:50:18 -0400

    task up to 7

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- Este script crea el usuario user_0d_1 con todos los privilegios en el servidor MySQL
+-- creating a user and granting privileges, the user have a password too
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
