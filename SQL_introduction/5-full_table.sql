6ecd6d0488dd563604244812cfe18920d169bc56
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-06-18 15:01:44 -0400

    fix code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,2 @@
 -- Este script imprime la descripción de la tabla first_table en la base de datos especificada
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    information_schema.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
