8ef3fb7e9df5a60b0d42f0dcc08eaa9c4d3380a1
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-06-18 16:22:27 -0400

    fix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Este script lista todos los privilegios de los usuarios user_0d_1 y user_0d_2 en localhost
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; -- Lista los privilegios de user_0d_1
-SHOW GRANTS FOR 'user_0d_2'@'localhost'; -- Lista los privilegios de user_0d_2
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost'; 
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
