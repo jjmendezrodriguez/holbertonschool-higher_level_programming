c47172bedea4d96cfc8220972218c6d4383cf3e8
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-06-20 14:54:38 -0400

    fix code add SQLAlchemy

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
+"""list states in database"""
 import MySQLdb
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """base of the sql"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(256), nullable=False)
+
+def list_states(username, password, dbname):
+    """connects to mysql database"""
+    # create a connection string
+    conn_str = f"mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}"
+
+    # create an engine
+    engine = create_engine(conn_str)
+
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # create a Session
+    session = Session()
+
+    # query all states and order by id
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    # print each state
+    for state in states:
+        print(f"({state.id}, '{state.name}')")
+
+    session.close()
 
 if __name__ == "__main__":
-    # Argumentos de línea de comandos
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+    else:
+        # get command line arg
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
 
-    # Conexión a la base de datos
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    # Crear un cursor para ejecutar las consultas
-    cursor = db.cursor()
-
-    # Ejecutar la consulta para listar todos los estados
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Obtener todos los resultados de la consulta
-    states = cursor.fetchall()
-
-    # Imprimir los resultados
-    for state in states:
-        print(state)
-
-    # Cerrar el cursor y la conexión a la base de datos
-    cursor.close()
-    db.close()
+        # call the function to list states
+        list_states(username, password, dbname)
