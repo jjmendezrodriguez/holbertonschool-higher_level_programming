f45813e1af9d7b381918c270a06161eef50f7e84
Josemendez <jjmendezrodriguez@yahoo.com>
2024-07-30 12:29:59 -0400

    task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,16 @@
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+const listMovies = document.getElementById('list_movies');
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      listMovies.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching the movie data:', error);
+  });
